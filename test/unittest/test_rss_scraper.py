13f0ee15bf512f0e2bfad380afd712c1fa7680a6
tharealbasti <sebastian.strasser98@web.de>
2022-04-04 15:48:04 +0200

    Issue #12: Fixes test for splitting RSS feeds

--- test/unittest/test_rss_scraper.py
+++ test/unittest/test_rss_scraper.py
@@ -3,16 +3,15 @@ import sys
 
 import requests_mock
 
-sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", ".."))
-from src.data_collectors import RssDataCollector
-from src.helper import split_rss_feed
-
-from test_data import expected_parsed_feed
+sys.path.insert(0, os.path.join(os.path.abspath(os.path.dirname(__file__)), "..", "..", "src"))
+from data_collectors import RssDataCollector
+from helper import split_rss_feed
 
 TEST_DATA_FOLDER = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test_data")
 PATH_FEED_DATABASE = os.path.join(TEST_DATA_FOLDER, 'example_feed_database.html')
 PATH_RAW_FEED = os.path.join(TEST_DATA_FOLDER, 'raw_feed.xml')
 
+REQUIRED_KEYS = ['title', 'title_detail', 'summary', 'summary_detail', 'links', 'link', 'id', 'guidislink', 'published', 'published_parsed', 'media_content', 'feed_source']
 
 TEST_CONFIG_RSS_REQUEST_HEADERS = {
         "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
@@ -30,17 +29,24 @@ TEST_CONFIG_RSS_REQUEST_HEADERS = {
         "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/99.0.4844.82 Safari/537.36"
     }
 
-@requests_mock.Mocker()
-def test_get_feed_urls(requests_mock):
+
+@requests_mock.Mocker(kw='mocked_request')
+def test_get_feed_urls(**kwargs):
     base_url = 'http://test.com'
-    with open(PATH_FEED_DATABASE, encoding='utf-8') as html_file:  
-        requests_mock.get(base_url, text=html_file.read())
+    with open(PATH_FEED_DATABASE, encoding='utf-8') as html_file:
+        response = html_file.read()
+        kwargs['mocked_request'].get(base_url, text=response)
 
     rss_data_collector = RssDataCollector(base_url=base_url, request_headers=TEST_CONFIG_RSS_REQUEST_HEADERS)
     feeds = rss_data_collector._get_feed_urls()
+    assert kwargs['mocked_request'].called
     assert len(feeds) == 5
 
-def test_split_rss_feeds(): 
-    with open(PATH_RAW_FEED) as raw_feed_file:
-        parsed_feed = split_rss_feed(raw_feed_file.read())
-        assert expected_parsed_feed.FEED_ENTRIES == parsed_feed
+def test_split_rss_feeds():
+    with open(PATH_RAW_FEED, 'r') as raw_feed_file:
+        raw_feed = raw_feed_file.read()
+        parsed_feed = split_rss_feed(raw_feed)
+        assert len(parsed_feed) == 2
+
+        for feed_entry in parsed_feed:
+            assert len(feed_entry.keys()) == len(REQUIRED_KEYS)
