18450fdd31c038aa00df453e39455c075662143e
dnzxy <d.c.cengiz@googlemail.com>
2022-04-18 16:38:22 +0200

    Additions: - Added .env files locally for each developer to be maintained - Changed constants.py functionality of getting sensitive data - Added .env file handling to workflow of github action - Added packages to requirements.txt

--- src/constants.py
+++ src/constants.py
@@ -1,3 +1,8 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
 # Constants for config
 CONFIG_KAFKA = 'Kafka'
 CONFIG_KAFKA_ENV_LOCAL = 'Local'
@@ -7,11 +12,12 @@ CONFIG_KAFKA_PORT = 'Port'
 CONFIG_GENERAL = 'General'
 CONFIG_GENERAL_MAX_WORKERS = 'MaxWorkers'
 CONFIG_RSS_HEADER = 'RssHeader'
-CONFIG_REDDIT_CLIENT_ID = 'RedditClientId'
-CONFIG_REDDIT_CLIENT_SECRET = 'RedditClientSecret'
-CONFIG_TWITTER_CONSUMER_KEY = 'TwitterConsumerKey'
-CONFIG_TWITTER_CONSUMER_SECRET = 'TwitterConsumerSecret'
-CONFIG_TWITTER_BEARER_TOKEN = 'TwitterBearerToken'
+
+REDDIT_CLIENT_ID = os.environ['REDDIT_CLIENT_ID']
+REDDIT_CLIENT_SECRET = os.environ['REDDIT_CLIENT_SECRET']
+TWITTER_CONSUMER_KEY = os.environ['TWITTER_CONSUMER_KEY']
+TWITTER_CONSUMER_SECRET = os.environ['TWITTER_CONSUMER_SECRET']
+TWITTER_BEARER_TOKEN = os.environ['TWITTER_BEARER_TOKEN']
 
 # Constants for argument parser
 DATA_SOURCE_RSS = 'rss'
